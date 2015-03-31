4a680d255273a7672a1e0e356b96935bd4e6b6f3
blunk <blunk@iaw.rub.de>
2015-03-31 14:10:03 +0200

    added logging for user leaves group and group is created hooks

--- hooks/class-aal-hook-buddypress.php
+++ hooks/class-aal-hook-buddypress.php
@@ -15,8 +15,36 @@ class AAL_Hook_BuddyPress extends AAL_Hook_Base {
         ) );
     }
 
+    public function hooks_aal_bp_leave_group ($group_id, $user_id) {
+        $group = groups_get_group( array( 'group_id' => $group_id ) );
+
+        aal_insert_log( array(
+            'action'      => 'left group',
+            'object_type' => 'BuddyPress',
+            'user_id'     => $user_id,
+            'object_id'   => $group_id,
+            'object_name' => $group->name,
+        ) );
+    }
+
+    public function hooks_aal_bp_group_created($group_id, $user_id) {
+        $group = groups_get_group( array( 'group_id' => $group_id ) );
+
+        aal_insert_log( array(
+            'action'      => 'created group',
+            'object_type' => 'BuddyPress',
+            'user_id'     => $user_id,
+            'object_id'   => $group_id,
+            'object_name' => $group->name,
+        ) );
+    }
+
     public function __construct(){
+        //TODO class exists for buddypress
         add_action('groups_join_group', array( &$this, 'hooks_aal_bp_join_group'), 5, 2);
+        add_action('groups_leave_group', array( &$this, 'hooks_aal_bp_leave_group'), 5, 2);
+
+        add_action('groups_group_create_complete', array( &$this, 'hooks_aal_bp_group_created'), 5, 2);
 
         parent::__construct();
     }
